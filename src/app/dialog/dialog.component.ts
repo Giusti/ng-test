45794eff1973265f34f4f4138a29dc2867843637
Christopher Giustiniani <giustiniani.christopher@gmail.com>
2018-09-27 21:29:42 +0200

    initial commit

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+import {MatDialog} from '@angular/material';
+
+@Component({
+  templateUrl: 'dialog.component.html',
+})
+export class DialogComponent {}
+
